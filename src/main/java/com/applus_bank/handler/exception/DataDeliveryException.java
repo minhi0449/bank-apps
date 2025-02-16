ca725de4e560cf06eb0087a4d28ef7cda9f0c5f6
minhi0449 <minhi0449@gmail.com>
2025-02-16 13:09:33 +0900

    git repo 폴더 구조 변경 test

--- src/main/java/com/applus_bank/handler/exception/DataDeliveryException.java
+++ src/main/java/com/applus_bank/handler/exception/DataDeliveryException.java
@@ -2,6 +2,18 @@ package com.applus_bank.handler.exception;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * 날짜 : 2025.02.14 (금)
+ * 이름 : 김민희
+ * 내용 : 데이터 전송 중 발생하는 예외를 처리하는 클래스
+ */
+
+/*
+    날짜 : 2025.02.14 (금)
+    이름 : 김민희
+    내용 : 데이터 전송 중 발생하는 예외를 처리하는 클래스
+ */
+
 // 사용자 정의 예외 클래스 만들기
 public class DataDeliveryException extends RuntimeException{
     private HttpStatus status;
@@ -9,6 +21,12 @@ public class DataDeliveryException extends RuntimeException{
     // 예외 발생했을 때, → HTTP 상태코드를 알려준다.
     // 어떤 예외 발생했는지, 상태 메세지를 알려준다.
     // 메세지 (어떤 예외 발생)
+
+    /**
+     *
+     * @param message
+     * @param status
+     */
     public DataDeliveryException (String message, HttpStatus status){
         super(message);
         this.status = status;
